85111c301d2018a6632f5ef33bc4f87fe608bfe7
MinoriNakamura <saintlyterritory526@gmail.com>
2019-07-15 11:10:25 +0900

    ゲームに必要かもしれないライブラリを作成するためのファイルを追加

---
+++ DirectX_Lib/DirectX_Lib/Draw.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+
+#define WIN32_LEAN_AND_MEAN             // Windows ヘッダーからほとんど使用されていない部分を除外する
+
+#include <windows.h>
+#include <d3d9.h>
+
+VOID Draw(float x, float y, float z, float rhw, DWORD color, float tu, float tv, float width, float height, float tu_width, float tv_height, int texture);
+
+struct CUSTOMVERTEX {
+	float	x, y, z;	// 頂点座標
+	float	rhw;	// 除算数
+	DWORD	Color;	// 頂点の色
+	float	tu, tv;	//　テクスチャ座標
+};
+
+class DirectX {
+public:
+	LPDIRECT3DDEVICE9 pD3Device;
+	LPDIRECT3DTEXTURE9 pTexture[256];
+};
+
+
---
+++ DirectX_Lib/DirectX_Lib/Draw_Lib.cpp
@@ -0,0 +1,26 @@
+﻿// DirectX_Lib.cpp : スタティック ライブラリ用の関数を定義します。
+//
+
+#include "pch.h"
+#include "Draw.h"
+
+VOID Draw(float x,float y,float z,float rhw,DWORD color,float tu,float tv,float width,float height,float tu_width,float tv_height,int texture,DirectX directx)
+{
+	CUSTOMVERTEX v[4] =
+	{
+	{x        , y         , z, rhw, color, tu           , tv		    },
+	{x + width, y         , z, rhw, color, tu + tu_width, tv            },
+	{x + width, y + height, z, rhw, color, tu + tu_width, tv + tv_height},
+	{x        , y + height, z, rhw, color, tu           , tv + tv_height}
+	};
+
+	directx.pD3Device->BeginScene();
+
+	directx.pD3Device->SetTexture(0, directx.pTexture[texture]);
+	
+	directx.pD3Device->DrawPrimitiveUP(D3DPT_TRIANGLEFAN, 2, v, sizeof(CUSTOMVERTEX));
+
+	directx.pD3Device->EndScene();
+
+	directx.pD3Device->Present(0, 0, 0, 0);
+}
